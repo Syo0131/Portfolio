46eaaed1b7c77b4d06e532940e7a97e907bb929e
Syo0131 <diazdaybelsamuel@gmail.com>
2026-01-22 10:45:43 -0400

    Refactor Header component structure and styles for improved readability; remove unnecessary console logs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+import { loadEnv } from 'vite';
+
+const env = loadEnv(process.env.NODE_ENV || 'development', process.cwd(), '');
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon()],
+  vite: {
+    build: {
+      sourcemap: env.SOURCE_MAP === 'true'
+    }
+  }
 });
